9db169531ff5c1b187cfded6a70a137dfc3d5e39
Daniyal-Imran09 <daniyalimran602@gmail.com>
2025-04-06 11:45:05 -0700

    changed folder path

--- server/vite.ts
+++ server/vite.ts
@@ -69,7 +69,7 @@ export async function setupVite(app: Express, server: Server) {
 
 export function serveStatic(app: Express) {
   // Update distPath to point to the dist folder in the root directory
-  const distPath = path.resolve(import.meta.dirname, "..", "dist");
+  const distPath = path.resolve(import.meta.dirname, "..", "dist/public");
 
   if (!fs.existsSync(distPath)) {
     throw new Error(
